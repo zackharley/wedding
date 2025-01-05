e3317de720071201c3ae3738d267aecda83a2832
Zack Harley <zackharley@gmail.com>
2025-01-05 17:45:31 -0500

    Update desktop homepage

---
+++ src/app/components/gallery.tsx
@@ -0,0 +1,110 @@
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
+
+import mooseDayImg from '../img/gallery-moose-day.webp';
+import worldCupImg from '../img/gallery-world-cup-2018.webp';
+import friendsWedding from '../img/gallery-carter-liz-wedding.webp';
+import { Box } from '@mui/material';
+
+type ImageDefinition = {
+  src: StaticImageData;
+  alt: string;
+};
+
+const galleryImages: ImageDefinition[] = [
+  {
+    src: mooseDayImg,
+    alt: 'Kaitlyn, Zackery, and their dog Moose the day they picked him up from the breeder.',
+  },
+  {
+    src: worldCupImg,
+    alt: "Kaitlyn and Zackery in Cannes celebrating France's victory over Croatia in the 2018 FIFA World Cup.",
+  },
+  {
+    src: worldCupImg,
+    alt: "Kaitlyn and Zackery in Cannes celebrating France's victory over Croatia in the 2018 FIFA World Cup.",
+  },
+  {
+    src: friendsWedding,
+    alt: "Kaitlyn and Zackery at their friends' wedding.",
+  },
+  {
+    src: mooseDayImg,
+    alt: 'Kaitlyn, Zackery, and their dog Moose the day they picked him up from the breeder.',
+  },
+  {
+    src: worldCupImg,
+    alt: "Kaitlyn and Zackery in Cannes celebrating France's victory over Croatia in the 2018 FIFA World Cup.",
+  },
+  {
+    src: worldCupImg,
+    alt: "Kaitlyn and Zackery in Cannes celebrating France's victory over Croatia in the 2018 FIFA World Cup.",
+  },
+  {
+    src: friendsWedding,
+    alt: "Kaitlyn and Zackery at their friends' wedding.",
+  },
+  {
+    src: mooseDayImg,
+    alt: 'Kaitlyn, Zackery, and their dog Moose the day they picked him up from the breeder.',
+  },
+  {
+    src: worldCupImg,
+    alt: "Kaitlyn and Zackery in Cannes celebrating France's victory over Croatia in the 2018 FIFA World Cup.",
+  },
+  {
+    src: worldCupImg,
+    alt: "Kaitlyn and Zackery in Cannes celebrating France's victory over Croatia in the 2018 FIFA World Cup.",
+  },
+  {
+    src: friendsWedding,
+    alt: "Kaitlyn and Zackery at their friends' wedding.",
+  },
+];
+
+const numberOfColumns = 2;
+
+const imagesByColumn = galleryImages.reduce((acc, img, index) => {
+  acc[index % numberOfColumns] = [...(acc[index % numberOfColumns] || []), img];
+  return acc;
+}, [] as ImageDefinition[][]);
+
+const gapSize = 1;
+
+export default function Gallery() {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      overflow="hidden"
+      maxHeight="100dvh"
+    >
+      <Box
+        display="grid"
+        gridTemplateColumns={`repeat(${numberOfColumns}, 1fr)`}
+        gap={gapSize}
+        justifyContent="center"
+      >
+        {imagesByColumn.map((images, index) => (
+          <Box
+            key={index}
+            display="flex"
+            flexDirection="column"
+            rowGap={gapSize}
+            mt={index === 0 ? 10 : 0}
+          >
+            {images.map((img) => (
+              <Image
+                key={img.alt}
+                src={img.src}
+                alt={img.alt}
+                style={{ height: 'auto', width: '100%' }}
+                placeholder="blur"
+              />
+            ))}
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import flowerDecoration from './img/flower-decoration.svg';
 import mobilePattern from './img/mobile-pattern.svg';
 import Image from 'next/image';
+import Gallery from './components/gallery';
 
 export default function Home() {
   return (
@@ -30,98 +31,108 @@ export default function Home() {
           alt="Floral pattern graphic"
           fill
           sizes="100vw"
-          style={{
-            objectFit: 'cover',
-          }}
+          style={{ objectFit: 'cover' }}
         />
       </Box>
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        sx={{
-          height: '100%',
-          maxHeight: '37.5rem',
-          textAlign: 'center',
-          py: 4,
-          px: 3,
-        }}
-      >
-        <Typography
-          component="h1"
-          fontSize="3rem"
-          lineHeight="3rem"
-          letterSpacing="0.2rem"
-          color="primary.contrastText"
-        >
-          SAVE
-          <br />
-          <Typography
-            component="span"
-            fontFamily="kingstonSignature, serif"
-            fontSize="2rem"
-            lineHeight="2rem"
-          >
-            the
-          </Typography>
-          <br />
-          DATE
-        </Typography>
+      <Container sx={{ display: 'flex', alignItems: 'center' }}>
         <Box
           sx={{
-            backgroundColor: 'primary.contrastText',
-            flexGrow: 1,
-            my: 4,
-            width: '0.125rem',
+            display: { xs: 'none', md: 'flex' },
+            flexBasis: { xs: '100%', md: '50%' },
+            maxWidth: { xs: '100%', md: '50%' },
           }}
-        />
-        <Typography
-          color="primary.contrastText"
-          fontSize="1.5rem"
-          lineHeight="1.5rem"
-          letterSpacing="0.15rem"
-          sx={{ mb: 1 }}
         >
-          KAITLYN & ZACK
-        </Typography>
-        <Typography
-          color="primary.contrastText"
-          fontSize="0.875rem"
-          lineHeight="0.875rem"
-          letterSpacing="0.0875rem"
-          sx={{ mb: 4 }}
+          <Gallery />
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          sx={{
+            height: '100%',
+            maxHeight: '37.5rem',
+            flexBasis: { xs: '100%', md: '50%' },
+            py: 4,
+            px: 3,
+            alignItems: { xs: 'center', md: 'end' },
+            textAlign: { xs: 'center', md: 'right' },
+          }}
         >
-          ARE GETTING MARRIED
-        </Typography>
-        <Typography
-          color="primary.contrastText"
-          fontSize="3rem"
-          lineHeight="3rem"
-          letterSpacing="0.3rem"
-          sx={{ mb: 6.5 }}
-        >
-          09∙13∙25
-        </Typography>
-        <Typography
-          color="primary.contrastText"
-          fontSize="0.875rem"
-          lineHeight="0.875rem"
-          letterSpacing="0.0875rem"
-          sx={{ mb: 4 }}
-        >
-          STONECROP ACRES / MORRISBURG ON
-        </Typography>
-        <Image src={flowerDecoration} alt="Flower decoration graphic" />
-        <Typography
-          color="primary.contrastText"
-          fontSize="0.6875rem"
-          lineHeight="0.6875rem"
-          letterSpacing="0.06875rem"
-          sx={{ mt: 4 }}
-        >
-          FORMAL INVITATION TO FOLLOW
-        </Typography>
-      </Box>
+          <Typography
+            component="h1"
+            fontSize="3rem"
+            lineHeight="3rem"
+            letterSpacing="0.2rem"
+            color="primary.contrastText"
+          >
+            SAVE
+            <br />
+            <Typography
+              component="span"
+              fontFamily="kingstonSignature, serif"
+              fontSize="2rem"
+              lineHeight="2rem"
+            >
+              the
+            </Typography>
+            <br />
+            DATE
+          </Typography>
+          <Box
+            sx={{
+              backgroundColor: 'primary.contrastText',
+              flexGrow: 1,
+              my: 4,
+              width: '0.125rem',
+            }}
+          />
+          <Typography
+            color="primary.contrastText"
+            fontSize="1.5rem"
+            lineHeight="1.5rem"
+            letterSpacing="0.15rem"
+            sx={{ mb: 1 }}
+          >
+            KAITLYN & ZACK
+          </Typography>
+          <Typography
+            color="primary.contrastText"
+            fontSize="0.875rem"
+            lineHeight="0.875rem"
+            letterSpacing="0.0875rem"
+            sx={{ mb: 4 }}
+          >
+            ARE GETTING MARRIED
+          </Typography>
+          <Typography
+            color="primary.contrastText"
+            fontSize="3rem"
+            lineHeight="3rem"
+            letterSpacing="0.3rem"
+            sx={{ mb: 6.5 }}
+          >
+            09∙13∙25
+          </Typography>
+          <Typography
+            color="primary.contrastText"
+            fontSize="0.875rem"
+            lineHeight="0.875rem"
+            letterSpacing="0.0875rem"
+            sx={{ mb: 4 }}
+          >
+            STONECROP ACRES / MORRISBURG ON
+          </Typography>
+          <Image src={flowerDecoration} alt="Flower decoration graphic" />
+          <Typography
+            color="primary.contrastText"
+            fontSize="0.6875rem"
+            lineHeight="0.6875rem"
+            letterSpacing="0.06875rem"
+            sx={{ mt: 4 }}
+          >
+            FORMAL INVITATION TO FOLLOW
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   );
 }
