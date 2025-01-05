0f10ec7ad2ef7b1061d9d9d112bdb2fae14c8615
Zack Harley <zackharley@gmail.com>
2025-01-05 17:45:40 -0500

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="K & Z" />
+      </head>
       <ThemeProviderWrapper>
         <Box
           component="body"
