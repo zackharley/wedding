0a8d41db4b8205afe37e8ea8ec4c1cf929fdb836
Zack Harley <zackharley@gmail.com>
2025-06-26 20:29:09 -0400

    Add RSVP redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/rsvp',
+        destination: 'https://tally.so/r/n0Y8MB',
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
